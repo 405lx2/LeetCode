26096f2a2ee2a0507744f7e55f1928a05478a91a
Liangx <405lx2@163.com>
2016-12-26 23:15:14 +0800

    21 Mer Two Sorted List

---
+++ Leetocde/21. Merge Two Sorted Lists.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(int x) : val(x), next(NULL) {}
+	
+};
+
+class Solution {
+public:
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+
+		ListNode preHead(0), *p = &preHead;
+		while (l1&&l2)
+		{
+			int value = l1->val > l2->val ? l1->val : l2->val;
+			p->next = new ListNode(value);
+			p = p->next;
+			l1 = (value == l1->val) ? l1->next : l1;
+			l2 = (value == l1->val) ? l2 : l2->next;
+
+		}
+		return preHead.next;
+	}
+};
+
